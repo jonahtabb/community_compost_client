1ab4bac79ea3386aa68912280887c6f86a186c45
jonahtabb <jtabbdev@gmail.com>
2021-09-21 21:21:56 -0400

    Updates to helper functions and admin dashboard

---
+++ src/helpers/dayConverters.tsx
@@ -0,0 +1,58 @@
+
+export function dayConverterNumToString(value: number) {
+    let dayString
+    switch (value) {
+        case 1:
+            dayString = "Sunday";
+            break;
+        case 2:
+            dayString = "Monday";
+            break;
+        case 3:
+            dayString = "Tuesday";
+            break;
+        case 4:
+            dayString = "Wednesday";
+            break;
+        case 5:
+            dayString = "Thursday";
+            break;
+        case 6:
+            dayString = "Friday";
+            break;
+        case 7:
+            dayString = "Saturday";
+            break;
+        default: dayString = "Must choose a valid day of the week"
+    }
+    return dayString
+}
+
+export function dayConverterStringToNum(value: string) {
+    let dayNum
+    switch (value) {
+        case "Sunday":
+            dayNum = 1;
+            break;
+        case "Monday":
+            dayNum = 2;
+            break;
+        case "Tuesday":
+            dayNum = 3;
+            break;
+        case "Wednesday":
+            dayNum = 4;
+            break;
+        case "Thursday":
+            dayNum = 5;
+            break;
+        case "Friday":
+            dayNum = 6;
+            break;
+        case "Saturday":
+            dayNum = 7;
+            break;
+        default: dayNum = "Must choose a valid day of the week"
+    }
+    return dayNum
+}
--- src/home_admin/AdminHome.tsx
+++ src/home_admin/AdminHome.tsx
@@ -1,16 +1,18 @@
 import '../App.css';
 import { Component } from "react";
 import { BrowserRouter as Switch, Route, Redirect, withRouter, RouteComponentProps } from "react-router-dom";
-
-import { getOwnAdminProfile, getOwnCommunityProfile, getOwnUserData } from '../helpers';
-import {AdminProfile, CommunityProfile, User} from '../types'
+import { getOwnAdminProfile, getOwnCommunityProfile, getOwnUserData, getAllCommunityMembers, getAllPickupGroups } from '../helpers';
+import {AdminProfile, CommunityProfile, User, MemberProfile, CommunityMembers, PickupGroups} from '../types'
 import { AdminDashboard } from '.';
+import ManagePickupGroups from './ManagePickupGroups';
 
 type AdminHomeProps = {} & RouteComponentProps
 type AdminHomeState = 
     {userData: User} & 
     {adminProfile: AdminProfile} &
-    {communityProfile: CommunityProfile}
+    {communityProfile: CommunityProfile} &
+    {communityMembers: CommunityMembers} &
+    {pickupGroups: PickupGroups}
 
 class AdminHome extends Component<AdminHomeProps, AdminHomeState>{
     constructor(props: AdminHomeProps){
@@ -30,26 +32,62 @@ class AdminHome extends Component<AdminHomeProps, AdminHomeState>{
             communityProfile:{
                 communityName: '',
                 communityDescription: ''
-            }
-
+            },
+            communityMembers: [],
+            pickupGroups: []
         }
     }
 
     async componentDidMount(){
         const token = localStorage.getItem("token")
+        const isAdmin = localStorage.getItem("isAdmin")
         if (token) {
+            //Get own user data
             const userResponse = await getOwnUserData(token)
             const {email, first_name, last_name} = userResponse.userData
 
+            //Get own admin profile data
             const adminResponse = await getOwnAdminProfile(token)
             const {secondary_email, phone, phone_type, bio } = adminResponse.adminProfile
 
+            //Get own community profile data
             const communityResponse = await getOwnCommunityProfile(token)
             const {name, description} = await communityResponse.communityProfile
 
+            //Get a list of all members belonging to own community
+            const membersResponse = await getAllCommunityMembers(token)
+            let communityMembers = await membersResponse.allMembers.map((member: any )=> (
+                {
+                    userId: member.User.id,
+                    email: member.User.email,
+                    firstName: member.User.first_name,
+                    lastName: member.User.last_name,
+                    phonePrimary: member.phone_primary,
+                    locationAddress1: member.location_address1,
+                    locationAddress2: member.location_address2,
+                    locationCity: member.location_city,
+                    locationName: member.location_name
+
+                }
+            ))
+
+            //Get a list of all pickup groups belonging to the community
+            const groupsResponse = await getAllPickupGroups(token)
+            let pickupGroups = await groupsResponse.pickupGroups.map((group: any) => (
+                {
+                    id: group.id,
+                    name: group.name,
+                    description: group.description,
+                    publicNotes: group.public_notes,
+                    startTime: group.start_time,
+                    endTime: group.end_time,
+                    day: group.day
+                }
+            ))
+
             this.setState({
                 userData:{
-                    email,
+                    email: email,
                     firstName: first_name,
                     lastName: last_name
                 },
@@ -62,7 +100,9 @@ class AdminHome extends Component<AdminHomeProps, AdminHomeState>{
                 communityProfile:{
                     communityName: name,
                     communityDescription: description
-                }
+                },
+                communityMembers: communityMembers,
+                pickupGroups: pickupGroups
             })
         }
 
@@ -81,6 +121,13 @@ class AdminHome extends Component<AdminHomeProps, AdminHomeState>{
                     <Route exact path ={`${this.props.match.path}/dashboard`}>     
                         <AdminDashboard 
                             communityProfile={this.state.communityProfile}
+                            communityMembers={this.state.communityMembers}
+                            pickupGroups={this.state.pickupGroups}
+                        />
+                    </Route>
+                    <Route exact path ={`${this.props.match.path}/groups`}>
+                        <ManagePickupGroups 
+                            pickupGroups = {this.state.pickupGroups}
                         />
                     </Route>
                 </Switch>
