b7e18cbe22d6f808dda18d729c13fbf8d6e0524c
jonahtabb <jtabbdev@gmail.com>
2021-09-30 18:07:10 -0400

    Configured for deployment

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,8 +4,8 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
     APIURL = 'http://localhost:3001';
     break;
-    case 'INSERT CLIENT URL HERE':
-    APIURL = 'https://jtabb-community-compost-server.herokuapp.com/'
+    case 'https://jtabb-community-compost.herokuapp.com':
+    APIURL = 'https://jtabb-community-compost-server.herokuapp.com'
 }
 
 let CLIENTURL = '';
@@ -14,8 +14,8 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
     CLIENTURL = 'http://localhost:3000';
     break;
-    case 'INSERT CLIENT URL HERE':
-    CLIENTURL = 'https://jtabb-yum-app-server.herokuapp.com'
+    case 'https://jtabb-community-compost.herokuapp.com':
+    CLIENTURL = 'https://jtabb-community-compost.herokuapp.com'
 }
 
 
